acbd8859aef2e5336c89c59a4733db1c0f45237e
vaibhavdesigns <vaisbhavdesignscode@gmail.com>
2026-09-10 15:13:52 +0530

    Updation of the Value

--- initializingthevalue.c
+++ initializingthevalue.c
@@ -3,8 +3,10 @@
 int main ()
 {
 // INITIALIZING //
+// VARIABLE //
 int a = 4  , b = 3 ;
 //  PRINTING THE VALUE OF initialized variables //
-printf("THE VALUE OF a and b ARE : %d \n");
+printf("THE VALUE OF a and b ARE : %d , %d \n", a , b);
 return 0 ;
 }
+
---
+++ updationofvalue.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main ()
+{
+    int x = 4 , y = 6 ;
+    printf("THE INITIALIZED VALUE OF TWO INTEGERS ARE : %d %d \n", x , y);
+    // UPDATION OF THE VALUE //
+    x += 46 ;
+    y += 44 ;
+    printf("THE UPDATED VALUE OF TWO INTEGERS ARE : %d %d \n", x , y);
+    return 0 ;
+}
