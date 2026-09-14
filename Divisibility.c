cd1464852d8b40acec49a241babf350014286fa1
vaibhavdesigns <vaibhavdesignscode@gmail.com>
2026-09-15 04:47:55 +0530

    Divisibility Test

---
+++ Divisibility.c
@@ -0,0 +1,17 @@
+// WAP TO CHECK WHETHER THE NUMBER IS DIVISIBLE BY 5 OR NOT //
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("ENTER THE NUMBER : \n");
+    scanf("%d", &num);
+    if (num % 5 == 0)
+    {
+        printf("THE NUMBER IS DIVISIBLE BY 5 \n");
+    }
+    else
+    {
+        printf("THE NUMBER IS NOT DIVISIBLE BY 5 \n");
+    }
+    return 0;
+}
