741df370a996e41237b3faf22e967a80bbdd234f
vaibhavdesigns <vaibhavdesignscode@gmail.com>
2026-09-17 10:27:17 +0530

    Profit Or Loss

---
+++ ProfitOrLoss.c
@@ -0,0 +1,20 @@
+// WAP TO DETERMINE WHETHER THE SHOPKEEPER HAS HAS MADE PROFIT OR LOSS //
+#include <stdio.h>
+int main()
+{
+    float cp, sp, profit, loss;
+    printf("ENTER THE COST PRICE : \n");
+    scanf("%.2f", &cp);
+    printf("ENTER THE SELLING PRICE : \n");
+    scanf("%.2f", &sp);
+
+    if (cp > sp)
+    {
+        printf("PROFIT\n");
+    }
+    else
+    {
+        printf("LOSS\n");
+    }
+    return 0;
+}
