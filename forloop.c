e9841793463cf5e09ead617d764524d8b60b1e4f
vaibhavdesigns <vaibhavdesignscode@gmail.com>
2026-09-25 06:18:02 +0530

    For Else Function

---
+++ forloop.c
@@ -0,0 +1,17 @@
+// WAP TO PRINT HELLO WORLD 100 TIMES //
+
+#include <stdio.h>
+
+int main()
+{
+    // INITIALIZATION WORKS FOR THE FIRST TIME TO START THE LOOP
+    // CONDITION IS CHECKED, THEN PRINTING HAPPENS,
+    // THEN VALUE UPDATES AND CONDITION IS CHECKED AGAIN
+
+    for (int i = 1; i <= 100; i = i + 1)
+    {
+        printf("HELLO WORLD \n");
+    }
+
+    return 0;
+}
